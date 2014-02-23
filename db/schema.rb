2e7c6f7799cab2923e773c09f4128e47b13be7ef
Sebastian Schwiecker <sebastian.schwiecker@gmail.com>
2014-02-23 23:48:23 +0100

    Add Flash messages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140223215324) do
+ActiveRecord::Schema.define(:version => 20140223222137) do
 
   create_table "rails_admin_histories", :force => true do |t|
     t.text     "message"
@@ -43,5 +43,13 @@ ActiveRecord::Schema.define(:version => 20140223215324) do
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+
+  create_table "vouchers", :force => true do |t|
+    t.string   "code"
+    t.decimal  "value",      :precision => 8, :scale => 2
+    t.boolean  "redeemed",                                 :default => false
+    t.datetime "created_at",                                                  :null => false
+    t.datetime "updated_at",                                                  :null => false
+  end
 
 end
