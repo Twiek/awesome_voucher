8f645cc961682c25ebf3d3833fc1161b915c27a6
Sebastian Schwiecker <sebastian.schwiecker@gmail.com>
2014-02-23 23:28:34 +0100

    Add validations to voucher

--- app/models/voucher.rb
+++ app/models/voucher.rb
@@ -1,3 +1,12 @@
 class Voucher < ActiveRecord::Base
   attr_accessible :code, :redeemed, :value
+
+  VALID_CODE_REGEX = /^[a-zA-Z\d*\-*]{1,8}$/
+  VALID_VALUE_REGEX = /^(\d{1,6})(\.|,)?(\d{1,2})?$/
+
+  validates :code,  presence: true, length: { maximum: 8 },
+                    format: { with: VALID_CODE_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :value, presence: true, format: { with: VALID_VALUE_REGEX },
+                    numericality: true
 end
